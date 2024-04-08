e6e1d34a152acd76c386735368a5b1a5a0ca7ff1
Elias Oliveira <eliassousaoliveira@hotmail.com>
2024-04-07 22:46:53 -0300

    Getters and Setters

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,18 @@
 class Cliente:
     def __init__(self, nome, telefone):
-        self.nome = nome
-        self.telefone = telefone
+        self._nome = nome
+        self._telefone = telefone
+
+    # métodos GET
+    def get_nome(self):
+        return self._nome
+
+    def get_telefone(self):
+        return self._telefone
+
+    # métodos SET
+    def set_nome(self, nome):
+        self._nome = nome
+
+    def set_telefone(self, telefone):
+        self._telefone = telefone
--- Conta.py
+++ Conta.py
@@ -3,3 +3,12 @@ class Conta:
         self.saldo = 0
         self.numero = numero
         self.titular = titular
+
+        def get_saldo(self):
+            return self.saldo
+
+        def set_saldo(self, saldo):
+            if (saldo >= 0):
+                self._saldo = saldo
+            else:
+                print("O saldo da conta não pode ser negativo")
